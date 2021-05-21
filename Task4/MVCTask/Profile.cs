7b18226cf1bcaa6f535d3b454d75cd7400fa1cb8
Hari1299 <75466867+Hari1299@users.noreply.github.com>
2021-05-21 19:10:46 +0530

    Add files via upload

---
+++ Task4/MVCTask/Profile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAssignment.Models
+{
+    public class Profile
+    { 
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Qualification { get; set; }
+        public bool IsEmployed { get; set; }
+        public int NoticePeriod { get; set; }
+        public double CurrentCtc { get; set; }
+    }
+}

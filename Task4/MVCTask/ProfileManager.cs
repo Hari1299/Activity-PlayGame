b67efc977c315ca2429fba7c4690cc511cc2a137
Hari1299 <75466867+Hari1299@users.noreply.github.com>
2021-05-21 19:12:52 +0530

    Add files via upload

---
+++ Task4/MVCTask/ProfileManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using MVCAssignment.Models;
+
+namespace MVCAssignment.Services
+{
+    public class ProfileManager : IProfile<Profile>
+    {
+        private ProfileContext _context;
+        private ILogger<ProfileManager> _logger;
+
+        public ProfileManager(ProfileContext context, ILogger<ProfileManager> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        public void Add(Profile t)
+        {
+            try
+            {
+                _context.profiles.Add(t);
+                _context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                _logger.LogDebug(e.Message);
+                throw;
+            }
+        }
+
+        public Profile Get(int id)
+        {
+            try
+            {
+                Profile profile = _context.profiles.FirstOrDefault(a => a.Id == id);
+                return profile;
+            }
+            catch (Exception e)
+            {
+                _logger.LogDebug(e.Message);
+            }
+            return null;
+        }
+
+        public IEnumerable<Profile> GetAll()
+        {
+            try
+            {
+                if (_context.profiles.Count() == 0)
+                    return null;
+                return _context.profiles.ToList();
+            }
+            catch (Exception e)
+            {
+                _logger.LogDebug(e.Message);
+            }
+            return null;
+        }
+    }
+}

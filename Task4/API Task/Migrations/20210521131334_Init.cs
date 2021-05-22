db9a124af6d90da529c3417ef5d053f1f2eb6b26
Hari1299 <75466867+Hari1299@users.noreply.github.com>
2021-05-22 06:56:19 +0530

    Add files via upload

---
+++ Task4/API Task/Migrations/20210521131334_Init.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APIAssingnment.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Weathers",
+                columns: table => new
+                {
+                    WeatherId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    highTemp = table.Column<double>(type: "float", nullable: false),
+                    lowTemp = table.Column<double>(type: "float", nullable: false),
+                    forcast = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weathers", x => x.WeatherId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Weathers");
+        }
+    }
+}

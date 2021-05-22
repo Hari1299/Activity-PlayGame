91f4d9103434ce96e50a30151d32f5b8fb849bf2
Hari1299 <75466867+Hari1299@users.noreply.github.com>
2021-05-22 06:46:54 +0530

    Add files via upload

---
+++ Task4/MVCTask/Migrations/20210521113352_Initial-Migration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCAssignment.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "profiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Qualification = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsEmployed = table.Column<bool>(type: "bit", nullable: false),
+                    NoticePeriod = table.Column<int>(type: "int", nullable: false),
+                    CurrentCtc = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_profiles", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "profiles",
+                columns: new[] { "Id", "Age", "CurrentCtc", "IsEmployed", "Name", "NoticePeriod", "Qualification" },
+                values: new object[] { 1, 20, 300000.0, true, "Hari", 60, "B.E CSE" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "profiles");
+        }
+    }
+}
